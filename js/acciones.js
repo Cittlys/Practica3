0adec5fa02a9e126a7617eca7338638091fb32f7
Cittlys <salvacit8@gmail.com>
2022-07-23 17:14:40 -0500

    Actualizacion 2

--- js/acciones.js
+++ js/acciones.js
@@ -1,10 +1,74 @@
 /*alert("Hi")*/
 
-function suma(){
-	var numero1 = parseFloat(document.getElementById('numero1').value);
-	var numero2 = parseFloat(document.getElementById('numero2').value);
-	var resultado = numero1 + numero2;
-	document.getElementById('resultado').value = resultado;
+(function() {
+	var formulario = document.getElementById("formulario"),
+
+	nombre = formulario.nombre,
+	correo = formulario.correo,
+	sexo = formulario.sexo,
+	terminos = formulario.terminos,
+	error = document.getElementById("error");
+
+	function validarNombre(e){
+
+		if (nombre.value == '' || nombre.value == null){
+			console.log("Porfavor completa el nombre");
+			error.style.display = "block";
+			error.innerHTML += "<li>Por favor completa el nombre</li>";
+
+			e.preventDefault();
+		}else{
+			error.style.display = "none";
+		}
+	} 
+
+	function validarCorreo(e){
+
+		if (correo.value == '' || correo.value == null){
+			console.log("Porfavor completa el correo");
+			error.style.display = "block";
+			error.innerHTML += "<li>Por favor completa el correo</li>";
+
+			e.preventDefault();
+		}else{
+			error.style.display = "none";
+		}
 	}
 
-	
+	function validarSexo(e){
+
+		if (sexo.value == '' || sexo.value == null){
+			console.log("Porfavor completa el sexo");
+			error.style.display = "block";
+			error.innerHTML += "<li>Por favor elige un sexo</li>";
+
+			e.preventDefault();
+		}else{
+			error.style.display = "none";
+		}
+	}
+
+	function validarTerminos(e){
+
+		if (terminos.checked == false) {
+			console.log("Porfavor acepta los terminos y condiciones");
+			error.style.display = "block";
+			error.innerHTML += "<li>Porfavor acepta terminos y condiciones</li>";
+
+			e.preventDefault();
+		}else{
+			error.style.display = "none";
+		}
+	}
+ 	
+ 	function validarFormulario(e) {
+ 		error.innerHTML = '';
+
+ 		validarNombre(e);
+ 		validarCorreo(e);
+ 		validarSexo(e);
+ 		validarTerminos(e);
+ 	}
+
+ 	formulario.addEventListener("submit",validarFormulario);
+}())
